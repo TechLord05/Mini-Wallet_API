e8e4f2ab976115b9258357095945327aa8767488
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2026-06-15 15:13:44 +0000

    feat: add express-validator for input validation and update API routes to v1

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use('/api/auth', authRoutes); // Use the authentication routes with the /api/auth prefix
-app.use('/api/wallet', walletRoutes);
+app.use('/api/v1/auth', authRoutes); // Use the authentication routes with the /api/auth prefix
+app.use('/api/v1/wallet', walletRoutes);
 
 app.get('/health', (req, res) => {
     res.json({ status: 'ok'});
