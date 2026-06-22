3fc5c6d40c305194d126f51c0a30d01f3badf2eb
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2026-06-22 15:09:55 +0000

    feat: add global rate limiting with @nestjs/throttler

--- mini-wallet-nest/src/app.module.ts
+++ mini-wallet-nest/src/app.module.ts
@@ -6,16 +6,30 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { WalletModule } from './wallet/wallet.module';
 import { PaymentModule } from './payment/payment.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     PrismaModule,
     AuthModule,
     WalletModule,
     PaymentModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
